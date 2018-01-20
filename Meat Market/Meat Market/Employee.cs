04626abe55018eba493c52675a9d28f459373d55
Divya palreddy <Divya palreddy@DIVYA>
2018-01-19 21:06:34 -0500

    Created New class

---
+++ Meat Market/Meat Market/Employee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Meat_Market
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+    }
+}
