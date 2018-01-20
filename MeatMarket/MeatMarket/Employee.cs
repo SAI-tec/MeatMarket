47df6f87c326622de34dfb41ae5bb24817643014
shash <shash@DESKTOP-H5ER5H9>
2018-01-19 21:08:49 -0500

    Made changes to Local Code

---
+++ MeatMarket/MeatMarket/Employee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MeatMarket
+{
+    public class Employee
+    {
+        public string EmployeeName { get; set; }
+    }
+}
