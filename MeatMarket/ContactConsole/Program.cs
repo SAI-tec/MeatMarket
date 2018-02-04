955dcf38e69d1472a8de7a9a19868ee8cc21591f
shash <shash@DESKTOP-H5ER5H9>
2018-02-04 12:19:34 -0500

    Code changes to Meat Market

---
+++ MeatMarket/ContactConsole/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer;
+
+namespace ContactConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                ContactUsDAL dal = new ContactUsDAL();
+                var t = dal.GetAll();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
