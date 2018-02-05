7b5c43556e910a65b017e66c4bac7d2447e6593b
shash <shash@DESKTOP-H5ER5H9>
2018-02-05 16:02:17 -0500

    Updated code for Meat market

---
+++ MeatMarket/MeatMarketSVC/Global.asax.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Routing;
+
+namespace MeatMarketSVC
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            //services 
+            //asp.net routing --> url --> Customer/Get/1
+            // routing module
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+    }
+}
